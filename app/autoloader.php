7245f28b22244af9d1b1a85766bad9c958e2d342
emilvo <emilveldhuizen@gmail.com>
2016-05-12 19:22:37 +0200

    - Made it easier to see if ur working at localhost -- Also made the code for using a database extern cleaner

--- app/autoloader.php
+++ app/autoloader.php
@@ -2,7 +2,6 @@
 spl_autoload_register(function($class_name) {
     $base_path = '../'.str_replace('\\', '/', strtolower($class_name).'.php');
     if (file_exists($base_path)) {
-        echo $base_path.'<br/>';
         include_once($base_path);
     }
 });
--- mvc/index.php
+++ mvc/index.php
@@ -4,4 +4,7 @@ include('../app/autoloader.php');
 use App\Core;
 
 $site = new Core($_GET);
+if ($_SERVER['SERVER_ADDR'] != '141.138.168.125') {
+    echo '<center><h3>LOCALHOST</h3></center>';
+}
 echo $site->getSite();
