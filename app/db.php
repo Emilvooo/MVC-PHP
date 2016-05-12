3e6409ae8d13b96e2d552640c4585517e352dfdd
emilvo <emilveldhuizen@gmail.com>
2016-05-12 19:23:25 +0200

    - Made the database code more dynamic

--- app/db.php
+++ app/db.php
@@ -7,14 +7,13 @@ class Db {
     public static function getInstance()
     {
         if (!isset(self::$instance)) {
-            if ($_SERVER['SERVER_ADDR'] == '141.138.168.125')
-            {
-                self::$instance = new \PDO('mysql:host=localhost;dbname=deb10192_emildb', 'deb10192_emil', 'hoi123');
+            if ($_SERVER['SERVER_ADDR'] == '141.138.168.125') {
+                $host = 'localhost';
             }
-            else
-            {
-                self::$instance = new \PDO('mysql:host=141.138.168.125;dbname=deb10192_emildb', 'deb10192_emil', 'hoi123');
+            else {
+                $host = '141.138.168.125';
             }
+            self::$instance = new \PDO('mysql:host='.$host.';dbname=deb10192_emildb', 'deb10192_emil', 'hoi123');
         }
         return self::$instance;
     }
