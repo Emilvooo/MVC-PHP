5d06e335583aecc70dedc48c30ccfb340dde216e
emilvo <emilveldhuizen@gmail.com>
2016-05-12 19:08:57 +0200

    - Can now connect with the database if i work with ampps

--- app/db.php
+++ app/db.php
@@ -4,11 +4,18 @@ namespace App;
 class Db {
     private static $instance = NULL;
 
-    public static function getInstance() {
+    public static function getInstance()
+    {
         if (!isset(self::$instance)) {
-            self::$instance = new \PDO('mysql:host=localhost;dbname=deb10192_emildb', 'deb10192_emil', 'hoi123');
+            if ($_SERVER['SERVER_ADDR'] == '141.138.168.125')
+            {
+                self::$instance = new \PDO('mysql:host=localhost;dbname=deb10192_emildb', 'deb10192_emil', 'hoi123');
+            }
+            else
+            {
+                self::$instance = new \PDO('mysql:host=141.138.168.125;dbname=deb10192_emildb', 'deb10192_emil', 'hoi123');
+            }
         }
         return self::$instance;
     }
-
 }
